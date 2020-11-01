88ae24b9721929876a3b74751e4713df341acdd0
Deep-anshu-Choudhary <51107735+Deep-anshu-Choudhary@users.noreply.github.com>
2020-11-01 23:59:56 +0530

    Add files via upload

---
+++ ImageGallery Folder/Image Gallery App/Image Gallery App/DataFetcher.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.IO;
+using System.Reflection;
+
+namespace Image_Gallery_App
+{
+    class DataFetcher
+    {
+
+        async Task<string> GetDatafromService(string searchstring)
+        {
+            string readText = null;
+            try
+            {
+                               
+                var azure = @"https://imagefetcher20200529182038.azurewebsites.net";
+                string url = azure + @"/api/fetch_images?query=" + searchstring + "&max_count=5";
+                using (HttpClient c = new HttpClient())
+                {
+                  readText = await c.GetStringAsync(url);
+                }
+               
+            }
+            catch
+            {
+                //string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),"sampleData.json");
+                
+                readText = File.ReadAllText(@"C:\Users\Deepanshu\Desktop\Grapecity Assignment\Installer and Working Guide\sampleData.json");
+            }
+            return readText;
+        }
+
+
+        public async Task<List<ImageItem>> GetImageData(string search)
+        {
+            string data = await GetDatafromService(search);
+            return JsonConvert.DeserializeObject<List<ImageItem>>(data);
+        }
+
+
+
+
+    }
+}
---
+++ ImageGallery Folder/Image Gallery App/Image Gallery App/ImageGallery.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using C1.Win.C1Tile;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace Image_Gallery_App
+{
+    public partial class ImageGallery : Form
+    {
+
+
+        DataFetcher datafetch = new DataFetcher();
+        List<ImageItem> imagesList;
+        int checkedItems = 0;
+
+
+        public ImageGallery()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+            Rectangle r = _searchBox.Bounds;
+            r.Inflate(3, 3);
+            Pen p = new Pen(Color.LightGray);
+            e.Graphics.DrawRectangle(p, r);
+
+
+
+        }
+
+        private async void _search_Click(object sender, EventArgs e)
+        {
+
+            
+            //ERROR Chances
+                statusStrip1.Visible = true;
+                imagesList = await datafetch.GetImageData(_searchBox.Text);
+                AddTiles(imagesList);
+                statusStrip1.Visible = false;
+            
+
+            //ERROR Chances
+
+             void AddTiles(List<ImageItem> imageList)
+            {
+                _imageTileControl.Groups[0].Tiles.Clear();
+                foreach (var imageitem in imageList)
+                {
+                    Tile tile = new Tile();
+                    tile.HorizontalSize = 2;
+                    tile.VerticalSize = 2;
+                    _imageTileControl.Groups[0].Tiles.Add(tile);
+                    Image img = Image.FromStream(new MemoryStream(imageitem.Base64));
+                    Template tl = new Template();
+                    ImageElement ie = new ImageElement();
+                    ie.ImageLayout = ForeImageLayout.Stretch;
+                    tl.Elements.Add(ie);
+                    tile.Template = tl;
+                    tile.Image = img;
+                }
+            }
+
+
+        }
+
+        private void _exportImage_Click(object sender, EventArgs e)
+        {
+            
+
+
+            List<Image> images = new List<Image>();
+            foreach (Tile tile in _imageTileControl.Groups[0].Tiles)
+            {
+                if (tile.Checked)
+                {
+                    images.Add(tile.Image);
+                }
+            }
+            ConvertToPdf(images);
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.DefaultExt = "pdf";
+            saveFile.Filter = "PDF files (*.pdf)|*.pdf*";
+
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                //OKD = Tomorrow.ToString("MMddyyyy");
+                imagePdfDocument.Save(saveFile.FileName);
+
+
+            }
+
+
+             
+
+
+        }
+
+
+        private void ConvertToPdf(List<Image> images)
+        {
+            RectangleF rect = imagePdfDocument.PageRectangle;
+            bool firstPage = true;
+            foreach (var selectedimg in images)
+            {
+                if (!firstPage)
+                {
+                    imagePdfDocument.NewPage();
+                }
+                firstPage = false;
+                rect.Inflate(-72, -72);
+                imagePdfDocument.DrawImage(selectedimg, rect);
+            }
+
+        }
+
+        private void _exportImage_Paint(object sender, PaintEventArgs e)
+        {
+
+            Rectangle r = new Rectangle(exportImage.Location.X,exportImage.Location.Y,exportImage.Width,exportImage.Height);
+            r.X -= 29;
+            r.Y -= 3;
+            r.Width--;
+            r.Height--;
+            Pen p = new Pen(Color.LightGray);
+            e.Graphics.DrawRectangle(p, r);
+            e.Graphics.DrawLine(p, new Point(0, 43), new Point(this.Width, 43));
+
+
+        }
+
+        private void _imageTileControl_TileChecked(object sender, C1.Win.C1Tile.TileEventArgs e)
+        {
+            checkedItems++;
+            exportImage.Visible = true;
+
+        }
+
+        private void _imageTileControl_TileUnchecked(object sender, C1.Win.C1Tile.TileEventArgs e)
+        {
+            checkedItems--;
+            exportImage.Visible = checkedItems > 0;
+
+        }
+
+        private void _imageTileControl_Paint(object sender, PaintEventArgs e)
+        {
+            Pen p = new Pen(Color.LightGray);
+            e.Graphics.DrawLine(p, 0, 43, 800, 43);
+
+
+        }
+
+        private void tile2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
